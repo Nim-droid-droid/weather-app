9b11d9bca894d9174ee7e35afef4bf97fb1971af
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-12-05 19:28:28 -0500

    Add forecast data queries

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ class Location {
                 return city.population > acc ? city : acc;
             }, 0)
 
-            console.log('LARGEST NEARBY CITY: ', largest_city);
+            console.log('LARGEST NEARBY CITY:', largest_city);
         })
     }
 }
@@ -39,21 +39,43 @@ const FORECAST_API_ENDPOINT = 'https://api.open-meteo.com/v1/forecast'
 class WeatherForecast {
     constructor(location) {
         this.location = location;
+        this.date = new Date();
         let options = {
             headers: {},
             mode: 'cors',
         };
-        this.forecast = fetch(`${FORECAST_API_ENDPOINT}?latitude=${this.location.lat}&longitude=${this.location.long}&hourly=temperature_2m,apparent_temperature,rain,showers,snowfall,weathercode`, options)
+        this.forecast = fetch(`${FORECAST_API_ENDPOINT}?timezone=auto&latitude=${this.location.lat}&longitude=${this.location.long}&hourly=temperature_2m,apparent_temperature,rain,showers,snowfall&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset`, options)
                         .then(response => response.json())
                         .catch(err => console.log(`Error: ${err}`));
+    }
+
+    getCurrentTemp() {
+        this.forecast.then(json => console.log("CURRENT TEMP:", json.hourly.temperature_2m[this.date.getHours()]))
+    }
+
+    getCurrentApparentTemp() {
+        this.forecast.then(json => console.log("CURRENT APPARENT TEMP:", json.hourly.apparent_temperature[this.date.getHours()]))
+    }
+
+    getTodaysSunrise() {
+        this.forecast.then(json => console.log("TODAYS SUNRISE:", json.daily.sunrise[0]))
+    }
+
+    getTodaysSunset() {
+        this.forecast.then(json => console.log("TODAYS SUNSET:", json.daily.sunset[0]))
     }
 }
 
 // Example city lookup by latitude/longitude
 loc = new Location("29.97944705", "31.13408231")
-loc.nearby_cities.then(json => console.log('NEARBY CITIES: ', json));
+loc.nearby_cities.then(json => console.log('NEARBY CITIES:', json));
 loc.getLargestNearbyCity();
 
 // Example forecast lookup
 weather = new WeatherForecast(loc);
-weather.forecast.then(json => console.log('FORECAST: ', json))
+weather.forecast.then(json => console.log('FORECAST:', json))
+
+weather.getCurrentTemp();
+weather.getCurrentApparentTemp();
+weather.getTodaysSunrise();
+weather.getTodaysSunset();
